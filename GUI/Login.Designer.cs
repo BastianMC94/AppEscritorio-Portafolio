2897dba42f8b694e324eb973d29535d861602c11
BastianMC94 <bmutis1774@gmail.com>
2017-10-18 19:16:03 -0300

    Capa Vista
    Cambio del Boton, agragamos imagen

--- GUI/Login.Designer.cs
+++ GUI/Login.Designer.cs
@@ -28,20 +28,37 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
-			// 
-			// Login
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 261);
-			this.Name = "Login";
-			this.Text = "Login";
-			this.Load += new System.EventHandler(this.Login_Load);
-			this.ResumeLayout(false);
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.BackColor = System.Drawing.Color.White;
+            this.metroTile1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.metroTile1.Location = new System.Drawing.Point(105, 177);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(62, 61);
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.TileImage = global::GUI.Properties.Resources.angle_double_right;
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.UseTileImage = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.metroTile1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
 
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+    }
 }
