7c625f7c56ba7fa72e996b36b84f66e73b49e18d
BastianMC94 <bmutis1774@gmail.com>
2017-10-25 19:26:19 -0300

    Excepciones y validacion Login mediante URL

--- GUI/Login.cs
+++ GUI/Login.cs
@@ -1,4 +1,5 @@
 ﻿using MetroFramework.Forms;
+using MetroFramework;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +9,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
 
 namespace GUI
 {
@@ -16,17 +20,53 @@ namespace GUI
 		public Login()
 		{
 			InitializeComponent();
+            txtCorreo.Select();
 		}
 
 		private void Login_Load(object sender, EventArgs e)
 		{
-
+           
 		}
 
-		private void btnIniciarSesion_Click(object sender, EventArgs e)
-		{
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            #region
+            //Validaciones
+            int count = 0;
 
-		}
+                if (txtCorreo.Text.Equals("") & txtContrasena.Text.Equals(""))
+                {
+                    MetroMessageBox.Show(this, "Debe ingresar sus credenciales", "Advertencia",
+                                         MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    count++;
+                    return;
+                }
+                if (txtCorreo.Text.Equals(""))
+                {
+                    MetroMessageBox.Show(this, "Ingrese su correo electrónico", "Advertencia",
+                                         MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    count++;
+                    return;
+                }
+                if (txtContrasena.Text.Equals(""))
+                {
+                    MetroMessageBox.Show(this, "Ingrese su contraseña", "Advertencia",
+                                         MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    count++;
+                    return;
+                }
+            #endregion //Validaciones
+            //Autentificar correo y clave, probar
+            HttpClient client = new HttpClient();
 
-	}
-}
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var byteArray = Encoding.ASCII.GetBytes("correo:clave");
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+            HttpResponseMessage response = client.GetAsync("http://portafoliapi.herokuapp.com/rest/encargadoCem/login").Result;
+        }
+
+        }
+    }
+
--- Solucion_Montreal/Programa.cs
+++ Solucion_Montreal/Programa.cs
@@ -8,6 +8,7 @@ namespace Solucion_Montreal
 {
     class Programa
     {
+        private string _programa_id;
         private string _nombre;
         private string _descripcion;
         private string _pais_destino;
@@ -16,15 +17,17 @@ namespace Solucion_Montreal
         private int _cupos;
         private char _programa_asignado;
 
-        public Programa(string _nombre, string _descripcion, string _pais_destino, string _fecha_inicio, string _fecha_termino, int _cupos, char _programa_asignado)
+        public string Programa_id
         {
-            this._nombre = _nombre;
-            this._descripcion = _descripcion;
-            this._pais_destino = _pais_destino;
-            this._fecha_inicio = _fecha_inicio;
-            this._fecha_termino = _fecha_termino;
-            this._cupos = _cupos;
-            this._programa_asignado = _programa_asignado;
+            get
+            {
+                return _programa_id;
+            }
+
+            set
+            {
+                _programa_id = value;
+            }
         }
 
         public string Nombre
@@ -116,6 +119,18 @@ namespace Solucion_Montreal
             {
                 _programa_asignado = value;
             }
+        }
+
+        public Programa(string _programa_id, string _nombre, string _descripcion, string _pais_destino, string _fecha_inicio, string _fecha_termino, int _cupos, char _programa_asignado)
+        {
+            this._programa_id = _programa_id;
+            this._nombre = _nombre;
+            this._descripcion = _descripcion;
+            this._pais_destino = _pais_destino;
+            this._fecha_inicio = _fecha_inicio;
+            this._fecha_termino = _fecha_termino;
+            this._cupos = _cupos;
+            this._programa_asignado = _programa_asignado;
         }
     }
 }
